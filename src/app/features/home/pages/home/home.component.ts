bee886ec8b440c83251cf22a3c5ea79746e77fec
Clara <clarazammar4@gmail.com>
2025-02-14 13:45:59 +0200

    Finish new arrivals section

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {HomeIntroComponent} from '../../components/home-intro/home-intro.component';
 import {BrowseCategoriesComponent} from '../../components/categories/components/browse-categories/browse-categories.component';
 import {BenefitsComponent} from '../../components/benefits/component/benefits-component/benefits.component';
+import {NewArrivalsComponent} from '../../components/new-arrivals/components/new-arrivals/new-arrivals.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HomeIntroComponent,
     BrowseCategoriesComponent,
-    BenefitsComponent
+    BenefitsComponent,
+    NewArrivalsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
