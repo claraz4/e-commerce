4419059b3ce539646f3468016d13c82dde69de11
Clara <clarazammar4@gmail.com>
2025-02-15 18:11:55 +0200

    Fix styling of navbar

--- src/app/core/navbar/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar/navbar.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { NavbarSearchComponent } from '../navbar-search/navbar-search.component';
+import {Router} from '@angular/router';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    NavbarSearchComponent
+    NavbarSearchComponent,
+    NgStyle
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  // Change navbar color when scrolling on home intro
+  backgroundColor = 'rgba(255, 255, 255, 1)';
 
+  // Change the opacity based on the position
+  changeOpacity() {
+    if (this.router.url === '/') {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const opacity = Math.min(scrollTop / 400 + 0.6, 1);
+      this.backgroundColor = `rgba(255, 255, 255, ${opacity})`;
+    }
+  }
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.changeOpacity();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.changeOpacity();
+  }
 }
--- src/app/features/home/components/home-intro/components/home-intro.component.ts
+++ src/app/features/home/components/home-intro/components/home-intro.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {ButtonComponent} from '../../../../../shared/buttons/button/button.component';
+import {NavbarComponent} from "../../../../../core/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-home-intro',
-  imports: [
-    ButtonComponent
-  ],
+    imports: [
+        ButtonComponent,
+        NavbarComponent
+    ],
   templateUrl: './home-intro.component.html',
   styleUrl: './home-intro.component.scss'
 })
