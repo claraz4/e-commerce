85c4f251f8bcad54dfc180530908dc5af8103d0f
Clara <clarazammar4@gmail.com>
2025-02-18 23:10:09 +0200

    Add auth guard for when the user is already logged in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './features/home/pages/home/home.component';
 import {SignInComponent} from './core/authentication/pages/sign-in/sign-in.component';
 import {SignUpComponent} from './core/authentication/pages/sign-up/sign-up.component';
+import {authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent, pathMatch: 'full' },
-  { path: 'sign-up', component: SignUpComponent, pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent, pathMatch: 'full', canActivate: [authGuard] },
+  { path: 'sign-up', component: SignUpComponent, pathMatch: 'full', canActivate: [authGuard] },
 ];
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,14 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+import {AuthService} from '../core/authentication/services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    router.navigate(['/']).then(() => {}); // Redirect if already logged in
+    return false;
+  }
+  return true
+};
