d7353e5b15f4ff90192fef2353ebf14e91077993
Clara <clarazammar4@gmail.com>
2025-03-09 19:25:21 +0200

    Fix first draft of products list responsiveness

--- src/app/shared/products/products-list/products-list.component.ts
+++ src/app/shared/products/products-list/products-list.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, SimpleChanges} from '@angular/core';
+import {Component, inject, Input, SimpleChanges} from '@angular/core';
 import {ButtonComponent} from '../../buttons/button/button.component';
 import {NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
 import {ProductBoxComponent} from '../product-box/product-box.component';
 import {IProductInfoDTO} from '../../../features/single-product/models/IProductInfoDTO';
 import {IProductInfo} from '../models/IProductInfo';
+import {BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-products-list',
@@ -18,12 +19,32 @@ import {IProductInfo} from '../models/IProductInfo';
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
   @Input({ required: true }) products!: IProductInfo[];
   @Input() isWishlist: boolean = false;
   @Input() title: string = "";
 
   startIndex: number = 0;
   elements: number = 3;
+
+  constructor() {
+    this.breakpointObserver.observe(['(max-width: 1024px)'])
+      .subscribe(result => {
+        if (result.matches) {
+          this.elements = 2;
+        }
+      });
+  }
+
+  ngOnInit() {
+    this.breakpointObserver.observe(['(max-width: 750px)'])
+      .subscribe(result => {
+        if (result.matches) {
+          this.elements = this.products.length;
+        }
+      });
+  }
 
   // Handle arrow clicks
   handlePrevClick() {
