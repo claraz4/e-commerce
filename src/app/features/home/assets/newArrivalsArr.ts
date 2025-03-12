14d9c3d50025f0aea392e7d89cd075306576bf12
Clara <clarazammar4@gmail.com>
2025-03-12 14:33:14 +0200

    Fix new arrivals img proportions

--- src/app/features/home/assets/newArrivalsArr.ts
+++ src/app/features/home/assets/newArrivalsArr.ts
@@ -1,6 +1,6 @@
 const newArrivalsArr: string[] = [
   "https://cdn.dummyjson.com/products/images/home-decoration/Decoration%20Swing/thumbnail.png",
-  "https://cdn.dummyjson.com/products/images/laptops/Apple%20MacBook%20Pro%2014%20Inch%20Space%20Grey/1.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Sofa/1.png",
   "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/thumbnail.png",
   "https://cdn.dummyjson.com/products/images/smartphones/Realme%20X/thumbnail.png"
 ]
