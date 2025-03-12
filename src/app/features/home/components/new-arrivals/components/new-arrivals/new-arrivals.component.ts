ce8aa6a182da6a1e298407faf20cb9fc08874bb7
Clara <clarazammar4@gmail.com>
2025-03-12 12:26:26 +0200

    Fix small part of responsiveness of new arrival Add pictures for new arrivals

--- src/app/features/home/components/new-arrivals/components/new-arrivals/new-arrivals.component.ts
+++ src/app/features/home/components/new-arrivals/components/new-arrivals/new-arrivals.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {newArrivalsArr} from '../../../../assets/newArrivalsArr';
 
 @Component({
   selector: 'app-new-arrivals',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class NewArrivalsComponent {
 
+  protected readonly newArrivalsArr = newArrivalsArr;
 }
